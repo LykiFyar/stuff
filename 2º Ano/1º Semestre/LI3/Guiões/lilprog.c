74c5ea0ed09001705dbb2fb474a568f8095199ca
LykiFyar <joaopvbbraga@gmail.com>
2021-10-29 17:48:04 +0100

    ;-;

--- 2º Ano/1º Semestre/LI3/Guiões/lilprog.c
+++ 2º Ano/1º Semestre/LI3/Guiões/lilprog.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct user {
     int id;
     char* login;
     char* type;
-    char* created_at;
+    char* created_at; // strptime !!
     int followers;
     int* follower_list;
     int following;
@@ -15,12 +16,13 @@ typedef struct user {
     int public_repos;
     } USER;
 
-int* make_list(char* s, int* v) {
+
+int* make_list(char s[], int* v) {
     int i = 0;
     if (*s == '[') {
         s += 1;
         while(*s) {
-            v = realloc(v,sizeof(int)*(i+1));
+            v = realloc(v,(sizeof(int))*(i+1));
             v[i] = atoi(strsep(&s, ","));
             i++;
         }
@@ -31,7 +33,7 @@ int* make_list(char* s, int* v) {
 
 void print_list(int v[], int N) {
     int i = 0;
-    while (i<N) {
+    while (i<N && v[i]) {
         printf("%d;",v[i]);
         i++;
     }
@@ -39,25 +41,26 @@ void print_list(int v[], int N) {
 
 void show_user(USER *user) {
     printf("-------------\nUser id: %d\nUser login: %s\nType of user: %s\nAccount created at: %s\nFollowers: %d\nIDs of followers: ",(*user).id,(*user).login,(*user).type,(*user).created_at,(*user).followers);
-    //print_list((*users).follower_list,(*users).followers);
+    print_list((*user).follower_list,(*user).followers);
     printf("\nFollowing: %d\nIDs of following: ",(*user).following);
-    //print_list((*users).following_list,(*users).following);
+    print_list((*user).following_list,(*user).following);
     printf("\nNumber of public gists: %d\nNumber of public repositories: %d\n",(*user).public_gists,(*user).public_repos);
 }
 
 USER init_user(char* info){
     struct user u;
-    int* followerlist = NULL, *followinglist = NULL;
+    u.follower_list = NULL, u.following_list = NULL;
     u.id = atoi(strsep(&info, ";"));
     u.login = strdup(strsep(&info, ";"));
     char* typeofuser = strsep(&info, ";");
     if (strcmp(typeofuser,"Bot") || strcmp(typeofuser,"User") || strcmp(typeofuser,"Organization")) u.type = strdup(typeofuser);
     else u.type = "UNKNOWN";
+
     u.created_at = strdup(strsep(&info, ";"));
     u.followers = atoi(strsep(&info, ";"));
-    u.follower_list = make_list(strdup(strsep(&info, "];")), followerlist);
+    u.follower_list = make_list(strdup(strsep(&info, "];")), u.follower_list);
     u.following = atoi(strsep(&info, ";"));
-    u.following_list = make_list(strdup(strsep(&info, "];")), followinglist);
+    u.following_list = make_list(strdup(strsep(&info, "];")), u.following_list);
     u.public_gists = atoi(strsep(&info, ";"));
     u.public_repos = atoi(info);
     return u;
@@ -73,13 +76,16 @@ int main() {
         return 1;
     }
     fgets(buffer,10000,data_file); // skip header
-    while(fgets(buffer,10000,data_file)) {
+    // while(fgets(buffer,10000,data_file)) {
+        while(i<13) {
+        fgets(buffer,10000,data_file);
         users = realloc(users, (i+1)*sizeof(USER));
         users[i] = init_user(buffer);
         i++;
+        
     }
     fclose(data_file);
-    for(int j=0; j<2; j++) { 
+    for(int j=0; j<100; j++) { 
         show_user(&(users[j]));
     }
     return 0;
