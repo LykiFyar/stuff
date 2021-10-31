20f2da702752885965a5cff8a07ac6a1bbe8553a
LykiFyar <joaopvbbraga@gmail.com>
2021-10-31 01:35:25 +0100

    more changes

--- 2º Ano/1º Semestre/LI3/Guiões/lilprog.c
+++ 2º Ano/1º Semestre/LI3/Guiões/lilprog.c
@@ -3,19 +3,52 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef enum {
+    Unknown,
+    User,
+    Bot,
+    Organization,    
+} TYPE;
+
 typedef struct user {
     int id;
     char* login;
-    char* type;
-    char* created_at; // strptime !!
+    TYPE type;
+    struct tm created_at;
     int followers;
     int* follower_list;
     int following;
     int* following_list;
     int public_gists;
     int public_repos;
-    } USER;
+    int is_valid;
+} USER;
 
+TYPE set_type(char* s) {
+    TYPE t;
+    if (strcmp(s,"User")) t = User;
+    else if (strcmp(s,"Bot")) t = Bot;
+    else if (strcmp(s,"Organization")) t = Organization;
+    else t = Unknown;
+    return t;
+}
+
+void print_type(TYPE t) {
+    if (t == User) printf("User");
+    else if (t == Bot) printf("Bot");
+    else if (t == Organization) printf("Organization");
+    else printf("Unknown");
+}
+
+struct tm convert_str2time(char* s) {
+    struct tm t = {0};
+    strptime(s, "%Y-%m-%d %T", &t);
+    return t;
+}
+
+void print_time(struct tm t) {
+    printf("%04d-%02d-%02d %02d:%02d:%02d",t.tm_year + 1900,t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec);
+}
 
 int* make_list(char *s, int* v) {
     int i = 0, t;
@@ -45,14 +78,20 @@ int* make_list(char *s, int* v) {
 
 void print_list(int *v, int N) {
     int i = 0;
-    while (i<N && v[i]) {
-        printf("%d;",v[i]);
-        i++;
+    if (v) {
+        while (i<N && v[i]) {
+            printf("%d;",v[i]);
+            i++;
+        }
     }
 }
 
 void show_user(USER *user) {
-    printf("-------------\nUser id: %d\nUser login: %s\nType of user: %s\nAccount created at: %s\nFollowers: %d\nIDs of followers: ",(*user).id,(*user).login,(*user).type,(*user).created_at,(*user).followers);
+    printf("-------------\nUser id: %d\nUser login: %s\nType of user: ",(*user).id,(*user).login);
+    print_type((*user).type);
+    printf("\nAccount created at: ");
+    print_time((*user).created_at);
+    printf("\nFollowers: %d\nIDs of followers: ",(*user).followers);
     print_list((*user).follower_list,(*user).followers);
     printf("\nFollowing: %d\nIDs of following: ",(*user).following);
     print_list((*user).following_list,(*user).following);
@@ -65,10 +104,8 @@ USER init_user(char* info){
     u.id = atoi(strsep(&info, ";"));
     u.login = strdup(strsep(&info, ";"));
     char* typeofuser = strsep(&info, ";");
-    if (strcmp(typeofuser,"Bot") || strcmp(typeofuser,"User") || strcmp(typeofuser,"Organization")) u.type = strdup(typeofuser);
-    else u.type = "UNKNOWN";
-
-    u.created_at = strdup(strsep(&info, ";"));
+    u.type = set_type(typeofuser);
+    u.created_at = convert_str2time(strdup(strsep(&info, ";")));
     u.followers = atoi(strsep(&info, ";"));
     char *flwrlist = strsep(&info, ";");
     u.follower_list = make_list(flwrlist, u.follower_list);
@@ -77,11 +114,12 @@ USER init_user(char* info){
     u.following_list = make_list(flwglist, u.following_list);
     u.public_gists = atoi(strsep(&info, ";"));
     u.public_repos = atoi(info);
+    u.is_valid = 0;
     return u;
 }
 
 int main() {
-    char buffer[100000];
+    char buffer[3000000];
     USER *users = malloc(sizeof(USER));
     int i=0;
     FILE *data_file = fopen("users.csv", "r");
@@ -89,8 +127,8 @@ int main() {
         printf("Error loading file\n");
         return 1;
     }
-    fgets(buffer,100000,data_file); // skip header
-    while(fgets(buffer,100000,data_file)) {
+    fgets(buffer,3000000,data_file); // skip header
+    while(fgets(buffer,3000000,data_file)) {
         users = realloc(users, (i+1)*sizeof(USER));
         users[i] = init_user(buffer);
         show_user(&(users[i]));
@@ -98,8 +136,5 @@ int main() {
         
     }
     fclose(data_file);
-   // for(int j=0; j<3000000; j++) { 
-     //   show_user(&(users[j]));
-    //}
     return 0;
 }
