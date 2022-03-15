dd4d5e18dd8f66d563dbcad1f9650fe40140f3dd
LykiFyar <joaopvbbraga@gmail.com>
2022-03-15 17:37:56 +0000

    lil update to POO

--- Yr2/Sem2/POO/Fichas/Ficha1/Ex3.java
+++ Yr2/Sem2/POO/Fichas/Ficha1/Ex3.java
@@ -1,7 +1,31 @@
 package Ficha1;
 
-public class Ex3 { // TODO
+import java.util.Scanner;
+
+public class Ex3 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        int N;
+        int i = 0;
+        int current;
+        int x1 = 0, x2 = 0, x3 = 0, x4 = 0;
+        System.out.println("Quantas classificacoes ira introduzir?");
+        N = input.nextInt();
+        while(i < N) {
+            System.out.println("Introduza um valor (0-20)"); 
+            current = input.nextInt();
+            if(current >= 0 && current <= 20) {
+                if(current < 5) x1++;
+                if(current >= 5 && current < 10) x2++;
+                if(current >= 10 && current < 15) x3++;
+                if(current >= 15) x4++;
+                i++;
+            }
+            else {
+                System.out.println("Valor inválido");
+            }
+        }
+        System.out.println("Numero de classificacoes entre:\n[0,5[: " + x1 + "\n[5,10[: " + x2 + "\n[10,15[: " + x3 + "\n[15,20]: " + x4);
+        input.close();
     }
 }
