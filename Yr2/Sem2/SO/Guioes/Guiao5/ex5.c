08b700063a559e00d4d6cadf400e5b08bff3c766
LykiFyar <joaopvbbraga@gmail.com>
2022-05-09 14:43:10 +0100

    Added ex5 SO Guiao5

--- Yr2/Sem2/SO/Guioes/Guiao5/ex5.c
+++ Yr2/Sem2/SO/Guioes/Guiao5/ex5.c
@@ -4,38 +4,96 @@
 #include <fcntl.h>
 #include <wait.h>
 
-/**
- * TODO - NOT DONE 
- */
+
 int main(int argc, char const *argv[]) {
-/* 
     int npipes = 3;
+    int nprocesses = 4;
     int fildes[npipes][2];
-    int *res = {-1,-1,-1};
+    int res = -1;
 
     for(int i = 0; i < npipes; i++) {
-        int res[i] = pipe(fildes[i]);
-    }
-
-        
+        res = pipe(fildes[i]);
         if(res == -1) {
             perror("Pipe is ded");
             return 1;
         }
+    }
+    for(int i = 0; i < nprocesses; i++) {
         int resf = fork();
-        switch (resf){
-            case -1:
-                perror("fork");   
+        switch(i) {
+            case 0: {
+                switch (resf){
+                    case -1:
+                        perror("fork");   
+                        break;
+                    case 0: // Filho
+                        close(fildes[i][0]); 
+                        dup2(fildes[i][1],1); 
+                        close(fildes[i][1]); 
+                        execlp("grep","grep", "-v", "^#", "/etc/passwd", NULL);
+                        break;
+                    default: // Pai
+                        close(fildes[i][1]); 
+                        break;
+                }
                 break;
-            case 0: // Filho
-                close(fildes[0]); 
-                dup2(fildes[1],1); 
-                close(fildes[1]); 
-                execlp("ls","ls", "/etc", NULL);
-                _exit(0);
+            }
+            case 1: {
+                switch (resf){
+                    case -1:
+                        perror("fork");   
+                        break;
+                    case 0: // Filho
+                        dup2(fildes[i-1][0], 0);
+                        close(fildes[i-1][0]); 
+                        dup2(fildes[i][1],1); 
+                        close(fildes[i][1]); 
+                        execlp("cut","cut", "-f7", "-d:", NULL);
+                        break;
+                    default: // Pai
+                        close(fildes[i-1][0]);
+                        close(fildes[i][1]);
+                        break;
+                }
                 break;
-            default: // Pai
+            }
+            case 2: {
+                switch (resf){
+                    case -1:
+                        perror("fork");   
+                        break;
+                    case 0: // Filho
+                        dup2(fildes[i-1][0], 0);
+                        close(fildes[i-1][0]); 
+                        dup2(fildes[i][1],1); 
+                        close(fildes[i][1]); 
+                        execlp("uniq","uniq", NULL);
+                        break;
+                    default: // Pai
+                        close(fildes[i-1][0]);
+                        close(fildes[i][1]);
+                        break;
+                }
                 break;
-        } */
+            }
+            case 3: {
+                switch (resf){
+                    case -1:
+                        perror("fork");   
+                        break;
+                    case 0: // Filho
+                        dup2(fildes[i-1][0],0); 
+                        close(fildes[i-1][0]); 
+                        execlp("wc","wc", "-l", NULL);
+                        break;
+                    default: // Pai
+                        close(fildes[i-1][0]);
+                        break;
+                }
+                break;
+            }
+        }
+    }
+    for(int i = 0; i < nprocesses; i++) wait(NULL);
     return 0;
 }
