77796f4e82f960866a16c60239f86832e105d77c
LykiFyar <joaopvbbraga@gmail.com>
2022-11-23 17:35:33 +0000

    Added ficha6 IA + introProlog

---
+++ Yr3/Sem1/IA/Práticas/Ficha6/Ficha6.pl
@@ -0,0 +1,52 @@
+
+%LICENCIATURA EM ENGENHARIA INFORMÁTICA
+%MESTRADO integrado EM ENGENHARIA INFORMÁTICA
+
+%Inteligência Artificial
+%2022/23
+
+%Draft Ficha 6
+
+
+% Extensao do predicado filho: Filho,Pai -> {V,F}
+
+filho( joao,jose ).
+filho( jose,manuel ).
+filho( carlos,jose ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado pai: Pai,Filho -> {V,F}
+
+pai( P,F ) :- filho( F,P ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado avo: Avo,Neto -> {V,F}
+
+avo( A,N ) :-
+    filho( N,X ),
+    filho( X,A ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado bisavo: Bisavo,Bisneto -> {V,F}
+
+bisavo( A,D ) :-
+    descendente( D,A,3 ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado descendente: Descendente,Ascendente -> {V,F}
+
+descendente( D,A ) :-
+    filho( D,A ).
+descendente( D,A ) :-
+    filho( D,X ),
+    descendente( X,A ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado descendente: Descendente,Ascendente,Grau -> {V,F}
+
+descendente( D,A,1 ) :-
+    filho( D,A ).
+descendente( D,A,G ) :-
+    G1 is G-1,
+    filho( D,X ),
+    descendente( X,A,G1 ).
