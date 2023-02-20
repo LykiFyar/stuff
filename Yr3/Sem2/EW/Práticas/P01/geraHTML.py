48e8b6f4bcff4c94234a55d64140cf51f7ed226f
LykiFyar <joaopvbbraga@gmail.com>
2023-02-20 14:39:31 +0000

    small changes EW P01

--- Yr3/Sem2/EW/Práticas/P01/geraHTML.py
+++ Yr3/Sem2/EW/Práticas/P01/geraHTML.py
@@ -1,70 +1,60 @@
 import json
 
-# TODO 
-# Adicionar ligações (lista) em cada cidade
-
-
-def ordCidade(c):
-    return c["nome"]
+def ordCidade(cidade):
+    return cidade['nome']
 
 f = open("mapa.json")
 mapa = json.load(f)
-cidades = mapa["cidades"]
-
+cidades = mapa['cidades']
 cidades.sort(key=ordCidade)
 
-pagHTML = """<!DOCTYPE html>
+pagHTML = """
+<!DOCTYPE html>
 <html>
     <head>
         <title>Mapa Virtual</title>
         <meta charset="utf-8"/>
     </head>
     <body>
-        <center>
-            <h1>Mapa Virtual</h1>
-        </center>
+        <h1>Mapa Virtual</h1>
         <table>
             <tr>
-                <!-- Índice -->
-                <td valign="top">
+                <!-- Coluna do índice -->
+                <td width="30%" valign="top">
                     <a name="indice"/>
                     <h3>Índice</h3>
-                    <ul>"""
+                    <ol>
+"""
 
 for c in cidades:
-    
-    pagHTML += f"""
-    <li>
-        <a href="#{c['id']}">{c["nome"]}</a>
-    </li>
-    """
-    
+    pagHTML += f"<li><a href='#{c['id']}'>{c['nome']}</a></li>"
+
+pagHTML += """
+</ol>
+                </td>
+                <!-- Coluna do conteúdo -->
+                <td width="70%">
+"""
+
 for c in cidades:
     pagHTML += f"""
-    <a name="{c["id"]}"/>
-    <h3>{c["nome"]}</h3>
-
-    <p><b>População: </b>{c["população"]}</p>
-    <p><b>Descrição: </b>{c["descrição"]}</p>
-    <p><b>Distrito: </b>{c["distrito"]}</p>
-    <address> </address>
-    <center>
-        <hr width="80%"/>
-    </center>
+                    <a name="{c['id']}"/>
+                    <h3>{c['nome']}</h3>
+                    <p><b>Distrito:</b> {c['distrito']}</p>
+                    <p><b>População:</b> {c['população']}</p>
+                    <p><b>Descrição:</b> {c['descrição']}</p>
+                    <address>[<a href="#indice">Voltar ao índice</a>]</address>
+                    <center>
+                        <hr width="80%"/>
+                    </center>
     """
 
-pagHTML += """<td>
-                    <a name="c1"/>
-                    <h3>Caminha</h3>
-
-                    <p><b>População: </b>354</p>
-                    <p><b>Descrição: </b>354</p>
-                    <p><b>Distrito: </b>354</p>
+pagHTML += """
                 </td>
             </tr>
         </table>
     </body>
-</html>"""
-    
-    
+</html>
+"""
+
 print(pagHTML)
